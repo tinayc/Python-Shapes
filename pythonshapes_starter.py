f3f858f570f2c0345844ee05667e29bb966ba14d
tinayc <tchen17@cps.edu>
2017-07-06 14:13:38 -0500

    Add files via upload

---
+++ pythonshapes_starter.py
@@ -0,0 +1,47 @@
+from turtle import *
+import math
+
+# Name your Turtle.
+t = Turtle()
+
+# Set Up your screen and starting position.
+
+x_pos = 0
+y_pos = 0
+t.setposition(x_pos, y_pos)
+
+### Write your code below:
+showturtle()
+begin_fill()
+pendown()
+forward(120)
+left(90)
+forward(120)
+left(90)
+forward(120)
+left(90)
+forward(120)
+left(90)
+
+color("violet")
+end_fill()
+penup()
+
+reset()
+begin_fill()
+pendown()
+for i in range(3):
+    forward(120)
+    left(120)
+
+
+color("red")
+end_fill()
+
+done()
+
+
+
+
+# Close window on click.
+exitonclick()
